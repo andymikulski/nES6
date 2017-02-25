4638d13ac02972cb5f0fe654214a146acbe9a54d
Andy Mikulski <amikulski@mozilla.com>
2017-02-24 22:09:45 -0700

    Cache serialisation functions

--- src/utils/serialisation.js
+++ src/utils/serialisation.js
@@ -1,24 +1,38 @@
+const uintArrayCache = {};
 export function uintArrayToString(uintArray) {
   if (!(uintArray instanceof Int32Array)) {
     throw new Error('uintArrayToString: Only accepts Int32Array parameter');
   }
-  var str = '';
-  for (var i = 0, strLen = uintArray.length; i < strLen; i++) {
-    var saveValue = uintArray[i];
-    if (saveValue > 0xFFFF) {
-      throw new Error("Invalid value attempted to be serialised");
+  const cacheKey = uintArray.toString();
+
+  if (!uintArrayCache[cacheKey]){
+    var str = '';
+    for (var i = 0, strLen = uintArray.length; i < strLen; i++) {
+      var saveValue = uintArray[i];
+      if (saveValue > 0xFFFF) {
+        throw new Error("Invalid value attempted to be serialised");
+      }
+      str += String.fromCharCode(saveValue);
     }
-    str += String.fromCharCode(saveValue);
+
+    uintArrayCache[cacheKey] = str;
   }
-  return str;
+
+  return uintArrayCache[cacheKey];
 };
 
+
+const stringCache = {};
 export function stringToUintArray(str) {
-  var buf = new Int32Array(str.length);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
-    buf[i] = str.charCodeAt(i) | 0;
+  if (!stringCache[stringCache]) {
+    var buf = new Int32Array(str.length);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+      buf[i] = str.charCodeAt(i) | 0;
+    }
+    stringCache[stringCache] = buf;
   }
-  return buf;
+
+  return stringCache[stringCache];
 };
 
 export function blobToString(blob) {
