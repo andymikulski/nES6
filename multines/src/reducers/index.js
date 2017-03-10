d1c96343bbf9b3dd3075feed8ae892303d06e4d9
Andy Mikulski <amikulski@mozilla.com>
2017-03-10 09:28:15 -0700

    Remove Multines
    Signed-off-by: Andy Mikulski <amikulski@mozilla.com>

--- multines/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-// import { routerReducer } from 'react-router-redux';
-
-import player from './playerReducer';
-
-export default combineReducers({
-  player,
-});
