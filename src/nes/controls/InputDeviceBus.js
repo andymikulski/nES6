babd37c15b4a10ee9875a46d97e2baaa7635f731
Andy Mikulski <amikulski@mozilla.com>
2017-03-10 11:12:09 -0700

    Removed hardcoded joypads
    Signed-off-by: Andy Mikulski <amikulski@mozilla.com>

--- src/nes/controls/InputDeviceBus.js
+++ src/nes/controls/InputDeviceBus.js
@@ -2,42 +2,32 @@ import Joypad from './Joypad';
 
 export default class InputDeviceBus {
   constructor() {
-    this.j1 = new Joypad();
-    this.j2 = new Joypad();
+    this.joyPads = {
+      0x4016: new Joypad(),
+      0x4017: new Joypad(),
+    };
   }
 
   getJoypad(index) {
-    switch (index) {
-      case 0:
-        return this.j1;
-      case 1:
-        return this.j2;
-      default:
-        return null;
-    }
+    return this.joyPads[Object.keys(this.joyPads)[index]];
   }
 
   writeToRegister(offset, data) {
-    switch (offset) {
-      case 0x4016:
-        this.j1.writeToRegister(offset, data);
-        break;
-      case 0x4017:
-        this.j2.writeToRegister(offset, data);
-        break;
+    const pad = this.joyPads[offset];
+
+    if (pad) {
+      pad.writeToRegister(data);
     }
   }
 
   readFromRegister(offset) {
-    var ret = 0;
-    switch (offset) {
-      case 0x4016:
-        ret = this.j1.readFromRegister(offset) | 0;
-        break;
-      case 0x4017:
-        ret = this.j2.readFromRegister(offset) | 0;
-        break;
+    let registerValue = 0;
+    const pad = this.joyPads[offset];
+
+    if (pad) {
+      registerValue = pad.readFromRegister() | 0;
     }
-    return ret;
+
+    return registerValue;
   }
 }
